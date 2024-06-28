88b4a82edb2658d41e8400af2fbc45155eb2e5a3
shivam-maker <shivampalpal20049@gmail.com>
2024-06-28 20:32:21 +0530

    Normalize line endings

---
+++ client/src/main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter as Router} from "react-router-dom";
+import {ThirdwebProvider} from "@thirdweb-dev/react"
+import { Sepolia } from "@thirdweb-dev/chains";
+
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
+import dotenv from "dotenv";
+dotenv.config();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <ThirdwebProvider activeChain={Sepolia} clientId= {process.env.CLIENT_ID}>
+        <Router>
+        <StateContextProvider>
+        <App/>
+
+        </StateContextProvider>
+        </Router>
+    </ThirdwebProvider>
+
+)
